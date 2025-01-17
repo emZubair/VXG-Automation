4162815fc634cf941c01fa8094d19a621dfa9c10
Zubair <36886070+emZubair@users.noreply.github.com>
2025-01-18 00:26:53 +0500

    Fixed the test cases (#1)

--- src/pageObjects/basePage.ts
+++ src/pageObjects/basePage.ts
@@ -1,33 +1,79 @@
-import { Page } from "@playwright/test";
+import { Locator, Page } from "@playwright/test";
 import { Timeout } from "../utils/enums";
 
 export class BasePage {
-    protected page: Page;
-    private readonly button: string = "[type='button']";
+  protected page: Page;
+  private readonly button: string = "[type='button']";
 
-    constructor(page: Page) {
-        this.page = page;
+  constructor(page: Page) {
+    this.page = page;
+  }
+  async visit(url: string): Promise<void> {
+    await this.page.goto(url, { waitUntil: "networkidle" });
+  }
+  async waitForReadiness(number = Timeout.THREE_SECONDS): Promise<void> {
+    return this.page.waitForTimeout(number);
+  }
+  async pressButton(buttonName: string): Promise<void> {
+    await this.page.locator(this.button).filter({ hasText: buttonName }).click({ force: true });
+  }
+  async waitForPageToLoad(timeout = 30000): Promise<void> {
+    await this.page.waitForLoadState("domcontentloaded", { timeout: timeout });
+  }
+  async waitForVisible(selector: string, timeout = 30000): Promise<void> {
+    try {
+      await this.page.waitForSelector(selector, { state: "visible", timeout });
+    } catch (error) {
+      console.log(
+        `Failed to find visible element with selector "${selector}" within ${timeout / 1000} seconds`,
+      );
+      throw error;
     }
-    async visit(url: string): Promise<void> {
-        await this.page.goto(url);
+  }
+
+  async clickButtonByLocator(
+    locator: string,
+    forced: boolean = true,
+    index: number = 0,
+  ): Promise<void> {
+    const button = this.page.locator(locator);
+    if (forced) {
+      await button.nth(index).click({ force: true });
     }
-    async waitForReadiness(number = Timeout.THREE_SECONDS): Promise<void> {
-        return this.page.waitForTimeout(number);
+  }
+  async getEmelemntByLocator(locator: string): Promise<Locator> {
+    return this.page.locator(locator);
+  }
+  async setValueByLocator(locator: string, value: string, index: number = 0): Promise<void> {
+    await (await this.getEmelemntByLocator(locator)).nth(index).fill(value);
+  }
+  async selectValueFromDropdown(locator: string, value: string, index: number = 0): Promise<void> {
+    const dropdown = (await this.getEmelemntByLocator(locator)).nth(index);
+    await dropdown.click({ force: true });
+    const option = this.page.locator(`text=${value}`);
+    await option.click({ force: true });
+  }
+  async getElementText(locator: string, index: number = 0): Promise<string> {
+    try {
+      const locatorHandle = this.page.locator(locator).nth(index);
+      const locatorText = await locatorHandle.textContent();
+      return locatorText?.trim() || "";
+    } catch (error) {
+      // If the locator is not found, return "not-found"
+      return "not-found";
     }
-    async pressButton(buttonName: string): Promise<void> {
-        await this.page.locator(this.button).filter({ hasText: buttonName }).click({ force: true });
+  }
+  async countCheckboxes(locator: string): Promise<number> {
+    const checkboxes = await this.page.$$(locator); // Select all matching elements
+    return checkboxes.length; // Return the count
+  }
+
+  async isLocatorVisible(locator: string): Promise<boolean> {
+    try {
+      await this.page.waitForSelector(locator, { state: "visible", timeout: 500 });
+      return true;
+    } catch (e) {
+      return false;
     }
-    async waitForPageToLoad(timeout = 30000): Promise<void> {
-        await this.page.waitForLoadState("domcontentloaded", { timeout: timeout });
-    }
-    async waitForVisible(selector: string, timeout = 30000): Promise<void> {
-        try {
-            await this.page.waitForSelector(selector, { state: "visible", timeout });
-        } catch (error) {
-            console.error(
-                `Failed to find visible element with selector "${selector}" within ${timeout / 1000} seconds`,
-            );
-            throw error;
-        }
-    }
+  }
 }
--- src/tests/TC_01_Verify_invalid_credentials_gives_expected_error_message.spec.ts
+++ src/tests/TC_01_Verify_invalid_credentials_gives_expected_error_message.spec.ts
@@ -1,59 +1,29 @@
 import type { Page } from "@playwright/test";
 import { LoginPage } from "../pageObjects/loginPage/login.po";
 import { expect, test } from "../fixtures/base-fixtures";
-import { loginTestData } from "../testData/loginTestData/loginTestData";
 import * as allure from "allure-js-commons";
+import { HomePage } from "../pageObjects/home/home.po";
 
 let page: Page;
+let homePage: HomePage;
 let loginPage: LoginPage;
 
 test.beforeAll(async ({ browser }) => {
-    page = await browser.newPage();
-    loginPage = new LoginPage(page);
+  page = await browser.newPage();
+  loginPage = new LoginPage(page);
+  homePage = new HomePage(page);
+  await loginPage.visit(process.env.BASE_URL);
 
-    await loginPage.visit(process.env.BASE_URL);
-    await Promise.all([
-        expect(await loginPage.getLogo()).toBeVisible(),
-        expect(await loginPage.getHeading()).toContainText(loginTestData.title),
-        expect(await loginPage.getEmailLabel()).toContainText(loginTestData.emailLabel),
-        expect(await loginPage.getPasswordLabel()).toContainText(loginTestData.passwordLabel),
-    ]);
+  await Promise.all([expect(await loginPage.getLogo()).toBeVisible()]);
 });
 
-test(
-    "Validate error message is displayed for invalid login credentials",
-    { tag: ["@smoke"] },
-    async (): Promise<void> => {
-        await allure.tags("smoke");
-        await allure.severity("critical");
-        await loginPage.signIn();
-        await Promise.all([
-            expect(await loginPage.getErrorMessage()).toContain(loginTestData.error.invalidEmail),
-            expect(await loginPage.getErrorMessage()).toContain(loginTestData.error.toShortPassword),
-        ]);
-        await loginPage.enterPassword(process.env.SYSTEM_PASSWORD);
-        await loginPage.signIn();
-        expect(await loginPage.getErrorMessage()).toContain(loginTestData.error.invalidEmail);
-
-        await page.reload();
-        await loginPage.enterEmail(loginTestData.invalidEmailAddress);
-        await loginPage.signIn();
-        page.on("dialog", async (dialog) => {
-            expect(dialog.message()).toBe(loginTestData.invalidEmailDialog(loginTestData.invalidEmailAddress));
-            await dialog.accept();
-        });
-
-        await loginPage.enterEmail(loginTestData.incompleteEmailAddress);
-        await loginPage.signIn();
-        page.on("dialog", async (dialog) => {
-            expect(dialog.message()).toBe(loginTestData.incompleteEmailDialog(loginTestData.incompleteEmailAddress));
-            await dialog.accept();
-        });
-        await loginPage.enterEmail(process.env.SYSTEM_USERNAME);
-        await loginPage.signIn();
-        expect(await loginPage.getErrorMessage()).toContain(loginTestData.error.toShortPassword);
-        await loginPage.enterPassword(loginTestData.invalidPassword);
-        await loginPage.signIn();
-        expect(await loginPage.getInvalidCredentialsError()).toContain(loginTestData.error.invalidCredentials);
-    },
-);
+test("Validate user is able to login", { tag: ["@smoke"] }, async (): Promise<void> => {
+  await allure.tags("smoke");
+  await loginPage.clickSignIn();
+  await loginPage.enterEmail(process.env.SYSTEM_USERNAME);
+  await loginPage.clickButtonByLocator(loginPage.emailContinueButton);
+  await loginPage.enterPassword(process.env.SYSTEM_PASSWORD);
+  await loginPage.clickButtonByLocator(loginPage.passwordContinueButton);
+  await loginPage.clickButtonByLocator(loginPage.linkRefuseButton, false);
+  await homePage.verifyPageIsLoaded();
+});
